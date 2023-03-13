301652257e3fd455e77c4671e1b7c6bced930add
Shivi7055 <shivikatiyar005@gmail.com>
2023-03-13 13:19:28 +0530

    Added Comment

--- DotProduct.py
+++ DotProduct.py
@@ -1,3 +1,12 @@
+'''Write a Python program that calculates the dot product of two lists containing the same number of elements. 
+The program should take two lists of integers as input from the user, and then call a function named dot_product to find the dot product of the two lists. 
+The dot_product function should take two lists a and b as input, and should return the dot product of those two lists.
+
+Your program should first prompt the user to enter the two lists of integers, which should be entered as strings separated by spaces.
+ Your program should then convert the input strings into lists of integers. Your program should then call the dot_product function with the two lists as arguments, and store the resulting dot product in a variable named result. 
+ Finally, your program should print out the dot product of the two lists as the output.'''
+
+
 def dotproduct(list1, list2):
   sum = 0
   for(i,j) in zip(list1, list2):
