8d181e36c28890fba6d5666115c0b9ed47baa31f
Shivangi Kumari <102220567+Shivi7055@users.noreply.github.com>
2023-03-13 12:30:17 +0530

    Add files via upload

---
+++ Transpose.py
@@ -0,0 +1,17 @@
+def Transpose(M):
+    # Initializing all elements of transpose matrix with 0 
+    transpose = [[0 for i in range(len(M))]for i in range(len(M))]    
+   	
+    for i in range(len(M)):
+        for j in range(len(M)):
+            transpose[i][j] =M[j][i] #copying elements from M to transpose matrix
+    return transpose
+
+n = int(input())
+M = [] 
+
+#Taking a matrix as input from the user 
+for i in range(n):
+    L = list(map(int, input().split()))
+    M.append(L)
+print(Transpose(M))
